90c807a03478f800c3e767dc363f8a75d95189c8
Jeff Gonis <jeff.gonis@gmail.com>
2016-03-02 21:49:27 -0700

    begin moving app into classes

--- script/mainthread/main.ts
+++ script/mainthread/main.ts
@@ -4,73 +4,32 @@ class AppMain {
     private _pointInput: HTMLInputElement;
     private _startButton: HTMLButtonElement;
     private _stopButton: HTMLButtonElement;
+    private _worker: Worker;
+    private _context: Window;
+    private _canvasWidth = -1;
+    private _canvasHeight = -1;
     
- constructor(   canvas: HTMLCanvasElement, 
-                kInput: HTMLInputElement, 
-                pointInput: HTMLInputElement,
-                startButton: HTMLButtonElement,
-                stopButton: HTMLButtonElement) {
-     this._canvas = canvas;
-     this._kInput = kInput;
-     this._pointInput = pointInput;
-     this._startButton = startButton;
-     this._stopButton = stopButton;
+ constructor( context: Window ) {
+     this._context = context;
+     this._canvas = <HTMLCanvasElement>this._context.document.getElementById("canvas");
+     this._kInput = <HTMLInputElement>this._context.document.getElementById("kValue");
+     this._pointInput = <HTMLInputElement>this._context.document.getElementById("point");
+     this._startButton = <HTMLButtonElement>this._context.document.getElementById("startButton");
+     this._stopButton = <HTMLButtonElement>this._context.document.getElementById("stopButton");     
      
      this.InitializeEventHandlers();
+     this.InitializeWorker();
+     
+     this._context.requestAnimationFrame(()=> { this.RecalcCanvas(this._canvas);});
  }   
  
  private InitializeEventHandlers() {
-     
+     this._stopButton.disabled = true;
  }
-}
-
-((context: Window) => {
-    context.onload = () => {
-        myInit();
-    }
-   
-    let count = 0;
-    let currentTotal = 0.0;
-    let canvasWidth = -1;
-    let canvasHeight = -1;
-    
-    function recalcCanvas() {
-        let canvas: HTMLCanvasElement = <HTMLCanvasElement>context.document.getElementById("canvas");
-        resizeCanvas(canvas);
-        
-        let ctx: CanvasRenderingContext2D = canvas.getContext("2d");        
-        context.requestAnimationFrame(()=> {recalcCanvas();});
-    }
-    
-    function resizeCanvas(canvas: HTMLCanvasElement) {
-        if(canvas.clientWidth !== canvasWidth || canvas.clientHeight !== canvasHeight)
-        {
-            canvas.width = canvas.clientWidth;
-            canvas.height = canvas.clientHeight;
-            canvasWidth = canvas.clientWidth;
-            canvasHeight = canvas.clientHeight;
-        }
-    }
-    
-    function paintImageToCanvas(imageArg: HTMLImageElement) {
-        let canvas: HTMLCanvasElement = <HTMLCanvasElement>context.document.getElementById("canvas");
-        resizeCanvas(canvas);
-        
-        let ctx: CanvasRenderingContext2D = canvas.getContext("2d");
-        ctx.drawImage(imageArg, 0, 0, canvas.width, canvas.height);
-    }
-    
-    function getImage() {
-        let image = new Image();
-        image.onload = (event: Event) => {
-            paintImageToCanvas(image);
-        };
-        image.src = "../img.jpg";
-    }
-    
-    function myInit() {
-        let myWorker = new Worker("build/worker.js");
-        myWorker.onmessage = (event) => {
+ 
+ private InitializeWorker() {
+     this._worker = new Worker("build/worker.js");
+        /*myWorker.onmessage = (event) => {
             if(event.data["type"] === 'start') {
                 console.log("start message received");
             } else if(event.data["type"] === "tick") {
@@ -78,8 +37,28 @@ class AppMain {
             }
         }
         myWorker.postMessage("start");        
-        context.requestAnimationFrame(()=> {recalcCanvas();});
-        context.setTimeout(getImage, 30);
+        context.requestAnimationFrame(()=> {recalcCanvas();});*/
+ }
+ 
+    private RecalcCanvas(canvas: HTMLCanvasElement) {
+        if(canvas.clientWidth !== this._canvasWidth || canvas.clientHeight !== this._canvasHeight)
+        {
+            canvas.width = canvas.clientWidth;
+            canvas.height = canvas.clientHeight;
+            this._canvasWidth = canvas.clientWidth;
+            this._canvasHeight = canvas.clientHeight;
+        }
+    }
+}
+
+((context: Window) => {
+    context.onload = () => {
+        myInit(context);
+    }
+    
+    let app: AppMain;
         
+    function myInit(context: Window) {
+        app = new AppMain(context);
     }
 })(window);
