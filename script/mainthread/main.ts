6717a21c10b4f0bad8dd7391ca0d94f93f6d7290
Jeff Gonis <jeffgonis@fastmail.com>
2016-03-04 12:13:32 -0700

    starting to build up with event handlers and drawing

--- script/mainthread/main.ts
+++ script/mainthread/main.ts
@@ -1,3 +1,32 @@
+class Point {
+    private _x: number = -1;
+    private _y: number = -1;
+    private _r: number = 0;
+    private _g: number = 0;
+    private _b: number = 0;
+    
+    
+    constructor(x: number, y: number, r: number, g: number, b: number) {
+        this._x = x;
+        this._y = y;
+        this._r = r;
+        this._g = g;
+        this._b = b;
+    }
+    
+    public X() {
+        return this._x;
+    }
+    
+    public Y() {
+        return this._y;
+    }
+    
+    public Color() {
+        return "rgb(" + this._r.toString()  + ", " + this._g.toString() + ", " + this._b.toString() + ")";
+    }
+}
+
 class AppMain {
     private _canvas: HTMLCanvasElement;
     private _kInput: HTMLInputElement;
@@ -6,8 +35,12 @@ class AppMain {
     private _stopButton: HTMLButtonElement;
     private _worker: Worker;
     private _context: Window;
-    private _canvasWidth = -1;
-    private _canvasHeight = -1;
+    private _canvasWidth: number = -1;
+    private _canvasHeight: number = -1;
+    private _numberOfMeans: number = -1;
+    private _numberOfPoints: number = -1;
+    private _points: Array<Point>;
+    
     
  constructor( context: Window ) {
      this._context = context;
@@ -20,11 +53,78 @@ class AppMain {
      this.InitializeEventHandlers();
      this.InitializeWorker();
      
-     this._context.requestAnimationFrame(()=> { this.RecalcCanvas(this._canvas);});
+     this._context.onresize = (event: UIEvent ) => { 
+         this._context.requestAnimationFrame( () => { 
+                    this.RecalcCanvas(this._canvas);
+                }) 
+            };
+    this.DetermineKValue();
+    this.DeterminePointValue();
+    this.GeneratePoints(this._numberOfPoints);
  }   
  
  private InitializeEventHandlers() {
+     this._startButton.onclick = (event: MouseEvent ) => { this.StartClicked(event);};
+     this._stopButton.onclick = (event: MouseEvent ) => { this.StopClicked(event);};
+     this._kInput.onchange = (event: Event) => {this.KValueChanged(event);};
+     this._pointInput.onchange = (event: Event) => {this.PointValueChanged(event); };
+ }
+ 
+ private StartClicked(event: MouseEvent) {
+     this._kInput.disabled = true;
+     this._pointInput.disabled = true;
+     this._stopButton.disabled = false;
+     this._startButton.disabled = true;
+ }
+ 
+ private StopClicked(event: MouseEvent) {
+     this._startButton.disabled = false;
+     this._pointInput.disabled = false;
+     this._kInput.disabled = false;
      this._stopButton.disabled = true;
+ }
+ 
+ private KValueChanged(event: Event) {
+     this.DetermineKValue();
+ }
+ 
+ private DetermineKValue() {
+     if(this._kInput.checkValidity() == true) {
+         this._numberOfMeans = this._kInput.valueAsNumber;
+         this._startButton.disabled = false;
+     } else {
+         this._startButton.disabled = true;
+     }
+ }
+ 
+ private PointValueChanged(event: Event) {
+     this.DeterminePointValue();
+ }
+ 
+ private DeterminePointValue() {
+     if(this._pointInput.checkValidity() == true) {
+         this._numberOfPoints = this._pointInput.valueAsNumber;
+         this._startButton.disabled = false;
+         this.GeneratePoints(this._numberOfPoints);
+     } else {
+         this._startButton.disabled = true;
+     }
+ }
+ 
+ private GeneratePoints(numberOfPoints: number) {
+    if(numberOfPoints > 0) {
+        this._points = new Array<Point>( numberOfPoints );
+    }
+    
+    for(let i: number = 0; i < this._points.length; i++) {
+         this._points[i] = new Point(0, 0, 255, 0, 0);
+    }
+    
+    this.DrawPoints(this._points);
+ }
+ 
+ private DrawPoints(points: Array<Point>) {
+     
  }
  
  private InitializeWorker() {
